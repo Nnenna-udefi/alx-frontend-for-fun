1eef5a9a152303ba90e004fdd7fc1044cfa32993
Nnenna udefi <miriamudefi@gmail.com>
2023-08-10 20:07:28 +0000

    docstrings

--- markdown2html.py
+++ markdown2html.py
@@ -5,6 +5,10 @@ import os
 
 
 if __name__ == '__main__':
+    """
+        sys.argv[1] = markdown file
+        sys.argv[2] = output file
+    """
     if len(sys.argv) < 2:
         print('Usage: ./markdown2html.py README.md README.html',
               file=sys.stderr)
