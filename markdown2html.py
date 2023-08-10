281c8b1d4f0d1956a45a2057c9fef8a8536c7299
Nnenna udefi <miriamudefi@gmail.com>
2023-08-10 22:01:06 +0000

    unorderd_list

--- markdown2html.py
+++ markdown2html.py
@@ -33,8 +33,29 @@ if __name__ == '__main__':
                 heading_text = match.group(2)
                 html_content += '<h{}>{}</h{}>\n'.format(level,
                                                          heading_text, level)
+
+            # check for unordered list
+            match_list = re.match(r"^\s*- (.*)$", line)
+            if match_list:
+                if not in_list:
+                    """
+                        intheList is a flag to track whether a list is being
+                        procesed and adds <ul> opening and closing tags
+                    """
+                    html_content += '<ul>\n'
+                    in_list = True
+                list_item = match_list.group(1)
+                html_content += "    <li>{}</li>".format(list_item)
+
             else:
+                if in_list:
+                    html_content += "</ul>\n"
+                    in_list = False
                 html_content += line
+
+        # Close any open unordered list at the end
+        if in_list:
+            html_content += "</ul>\n"
 
     # Write the HTML output to a file
     with open(output_file, "w", encoding="utf-8") as html:
