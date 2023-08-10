f367b7b40a316e928e807b4f0fea3d5d2b94c587
Nnenna udefi <miriamudefi@gmail.com>
2023-08-10 20:00:30 +0000

    pycode

--- markdown2html.py
+++ markdown2html.py
@@ -3,12 +3,15 @@
 import sys
 import os
 
-if len(sys.argv) < 2:
-    print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
-    exit(1)
-if not os.path.isfile(sys.argv[1]):
-    print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
-    exit(1)
-else:
-    pass
-    exit(0)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
+        exit(1)
+    if not os.path.isfile(sys.argv[1]):
+        print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
+        exit(1)
+    else:
+        pass
+        exit(0)
